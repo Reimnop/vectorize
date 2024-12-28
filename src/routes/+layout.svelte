786a63710d00e97f5b1dbda3145cf512f78b2f67
Reimnop <rayngu2005@gmail.com>
2024-12-28 18:58:59 +0700

    Add Open Graph meta tags for improved sharing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,12 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+    <meta property="og:title" content="Vectorize" />
+    <meta property="og:description" content=" THE tool to convert SVG files to Project Arrhythmia prefabs" />
+    <meta property="og:url" content="https://vectorize.reimnop.com/" />
+</svelte:head>
+
 <div class="w-screen min-h-screen p-4 flex flex-col gap-4">
 	<header>
 		<Header />
