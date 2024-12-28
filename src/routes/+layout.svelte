d51eb3dbe50f756dd41a6d7305ae96c4a45b389d
Reimnop <rayngu2005@gmail.com>
2024-12-28 19:10:37 +0700

    Enhance Open Graph meta tags for better SEO and sharing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,9 +7,13 @@
 </script>
 
 <svelte:head>
-    <meta property="og:title" content="Vectorize" />
-    <meta property="og:description" content=" THE tool to convert SVG files to Project Arrhythmia prefabs" />
-    <meta property="og:url" content="https://vectorize.reimnop.com/" />
+	<meta property="canonical" content="https://vectorize.reimnop.com/" />
+	<meta property="og:type" content="website" />
+	<meta property="og:locale" content="en_US" />
+	<meta property="og:site_name" content="Vectorize" />
+  <meta property="og:title" content="Vectorize" />
+  <meta property="og:description" content="THE tool to convert SVG files to Project Arrhythmia prefabs" />
+  <meta property="og:url" content="https://vectorize.reimnop.com/" />
 </svelte:head>
 
 <div class="w-screen min-h-screen p-4 flex flex-col gap-4">
