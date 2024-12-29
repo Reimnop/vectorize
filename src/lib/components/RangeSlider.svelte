c740a7b2f115d152d27198664ec56100293c20b9
Reimnop <rayngu2005@gmail.com>
2024-12-29 20:58:58 +0700

    Enhance RangeSlider for touch support and improve styling

--- src/lib/components/RangeSlider.svelte
+++ src/lib/components/RangeSlider.svelte
@@ -21,14 +21,18 @@
   onMount(() => {
     if (browser) {
       window.addEventListener("mouseup", mouseUp);
+      window.addEventListener("touchend", touchEnd);
       window.addEventListener("mousemove", mouseMove);
+      window.addEventListener("touchmove", touchMove);
     }
   });
 
   onDestroy(() => {
     if (browser) {
       window.removeEventListener("mouseup", mouseUp);
+      window.removeEventListener("touchend", touchEnd);
       window.removeEventListener("mousemove", mouseMove);
+      window.removeEventListener("touchmove", touchMove);
     }
   });
 
@@ -39,7 +43,15 @@
     dragging = true;
   }
 
+  function touchStart() {
+    dragging = true;
+  }
+
   function mouseUp() {
+    dragging = false;
+  }
+
+  function touchEnd() {
     dragging = false;
   }
 
@@ -55,6 +67,18 @@
     value = Math.round(newValue);
   }
 
+  function touchMove(event: TouchEvent) {
+    if (!slider) return;
+    if (!dragging) return;
+
+    const rect = slider.getBoundingClientRect();
+    const x = event.touches[0].clientX - rect.left;
+    const width = rect.width;
+    const newValue = Math.min(Math.max(0, x / width), 1) * (max - min) + min;
+
+    value = Math.round(newValue);
+  }
+
   function keyPress(event: KeyboardEvent) {
     if (event.key === "ArrowLeft") {
       value = Math.max(min, value - 1);
@@ -64,9 +88,10 @@
   }
 </script>
 
-<div class="flex flex-row gap-4 select-none {className}">
+<div class="flex flex-row gap-2 select-none {className}">
   <div bind:this={slider}
        onmousedown={mouseDown}
+       ontouchstart={touchStart}
        onkeydown={keyPress}
        draggable="false"
        role="slider" 
@@ -75,13 +100,14 @@
        aria-valuemin={min} 
        aria-valuemax={max} 
        class="relative w-full h-6 cursor-pointer 
-              [&>*:nth-child(1)]:hover:border-foreground 
-              [&>*:nth-child(2)]:hover:bg-foreground 
-              [&>*:nth-child(3)]:hover:bg-foreground 
-              [&>*:nth-child(3)]:hover:h-6">
+              [&>div:nth-child(1)]:hover:border-foreground 
+              [&>div:nth-child(2)]:hover:bg-foreground 
+              [&>div:nth-child(3)]:hover:bg-foreground 
+              [&>div:nth-child(3)]:hover:h-6">
     <div class="absolute top-1/2 -translate-y-1/2 w-full h-2 border border-foreground-sub duration-75 rounded-full"></div>
     <div class="absolute top-1/2 -translate-y-1/2 h-2 bg-foreground-sub duration-75 rounded-full" style={`width: ${(value - min) / (max - min) * 100}%`}></div>
-    <div class="absolute top-1/2 -translate-y-1/2 -translate-x-1/2 w-4 h-4 bg-foreground-sub duration-75 rounded-full" style={`left: ${(value - min) / (max - min) * 100}%`}></div>
+    <div class="absolute top-1/2 -translate-y-1/2 -translate-x-1/2 w-4 h-4 bg-foreground-sub duration-75 rounded-full" 
+         style="left: calc({(value - min) / (max - min) * 100}% - {(value - min) / (max - min) - 0.5}rem)"></div>
   </div>
   <span class="text-foreground-sub w-8 text-center border border-foreground-sub rounded-md">{value}</span>
 </div>
